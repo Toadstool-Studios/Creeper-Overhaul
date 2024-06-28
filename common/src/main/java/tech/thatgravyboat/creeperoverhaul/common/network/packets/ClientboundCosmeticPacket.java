213280246a4ec5a78f6151c2537de7d68174309d
ThatGravyBoat <thatgravyboat@gmail.com>
2024-06-28 21:22:26 -0230

    Fix packet names colliding

--- common/src/main/java/tech/thatgravyboat/creeperoverhaul/common/network/packets/ClientboundCosmeticPacket.java
+++ common/src/main/java/tech/thatgravyboat/creeperoverhaul/common/network/packets/ClientboundCosmeticPacket.java
@@ -3,6 +3,7 @@ package tech.thatgravyboat.creeperoverhaul.common.network.packets;
 import com.teamresourceful.bytecodecs.base.ByteCodec;
 import com.teamresourceful.resourcefullib.common.network.Packet;
 import com.teamresourceful.resourcefullib.common.network.base.ClientboundPacketType;
+import com.teamresourceful.resourcefullib.common.network.base.NetworkHandle;
 import com.teamresourceful.resourcefullib.common.network.base.PacketType;
 import com.teamresourceful.resourcefullib.common.network.defaults.CodecPacketType;
 import it.unimi.dsi.fastutil.objects.Object2BooleanMap;
@@ -16,33 +17,21 @@ public record ClientboundCosmeticPacket(
     Object2BooleanMap<UUID> players
 ) implements Packet<ClientboundCosmeticPacket> {
 
-    public static final ClientboundPacketType<ClientboundCosmeticPacket> TYPE = new Handler();
+    private static final ByteCodec<Object2BooleanMap<UUID>> UUID_TO_BOOL = ByteCodec.mapOf(ByteCodec.UUID, ByteCodec.BOOLEAN)
+            .map(Object2BooleanOpenHashMap::new, Object2BooleanOpenHashMap::new);
+
+    public static final ClientboundPacketType<ClientboundCosmeticPacket> TYPE = CodecPacketType.Client.create(
+            Creepers.id("cosmetics"),
+            UUID_TO_BOOL.map(ClientboundCosmeticPacket::new, ClientboundCosmeticPacket::players),
+            NetworkHandle.handle(message -> {
+                for (var entry : message.players().object2BooleanEntrySet()) {
+                    Cosmetics.setCosmeticShown(entry.getKey(), entry.getBooleanValue());
+                }
+            })
+    );
 
     @Override
     public PacketType<ClientboundCosmeticPacket> type() {
         return TYPE;
-    }
-
-    private static class Handler extends CodecPacketType<ClientboundCosmeticPacket> implements ClientboundPacketType<ClientboundCosmeticPacket> {
-
-        private static final ByteCodec<Object2BooleanMap<UUID>> UUID_TO_BOOL = ByteCodec.mapOf(ByteCodec.UUID, ByteCodec.BOOLEAN)
-                .map(Object2BooleanOpenHashMap::new, Object2BooleanOpenHashMap::new);
-
-        public Handler() {
-            super(
-                    ClientboundCosmeticPacket.class,
-                    Creepers.id("cosmetic"),
-                    UUID_TO_BOOL.map(ClientboundCosmeticPacket::new, ClientboundCosmeticPacket::players)
-            );
-        }
-
-        @Override
-        public Runnable handle(ClientboundCosmeticPacket message) {
-            return () -> {
-                for (var entry : message.players().object2BooleanEntrySet()) {
-                    Cosmetics.setCosmeticShown(entry.getKey(), entry.getBooleanValue());
-                }
-            };
-        }
     }
 }
--- common/src/main/java/tech/thatgravyboat/creeperoverhaul/common/network/packets/ServerboundCosmeticPacket.java
+++ common/src/main/java/tech/thatgravyboat/creeperoverhaul/common/network/packets/ServerboundCosmeticPacket.java
@@ -2,44 +2,25 @@ package tech.thatgravyboat.creeperoverhaul.common.network.packets;
 
 import com.teamresourceful.bytecodecs.base.ByteCodec;
 import com.teamresourceful.resourcefullib.common.network.Packet;
-import com.teamresourceful.resourcefullib.common.network.base.ClientboundPacketType;
+import com.teamresourceful.resourcefullib.common.network.base.NetworkHandle;
 import com.teamresourceful.resourcefullib.common.network.base.PacketType;
 import com.teamresourceful.resourcefullib.common.network.base.ServerboundPacketType;
 import com.teamresourceful.resourcefullib.common.network.defaults.CodecPacketType;
-import it.unimi.dsi.fastutil.objects.Object2BooleanMap;
-import it.unimi.dsi.fastutil.objects.Object2BooleanOpenHashMap;
-import net.minecraft.world.entity.player.Player;
 import tech.thatgravyboat.creeperoverhaul.Creepers;
-import tech.thatgravyboat.creeperoverhaul.client.cosmetics.Cosmetics;
 import tech.thatgravyboat.creeperoverhaul.common.utils.ServerCosmetics;
-
-import java.util.UUID;
-import java.util.function.Consumer;
 
 public record ServerboundCosmeticPacket(
     boolean shown
 ) implements Packet<ServerboundCosmeticPacket> {
 
-    public static final ServerboundPacketType<ServerboundCosmeticPacket> TYPE = new Handler();
+    public static final ServerboundPacketType<ServerboundCosmeticPacket> TYPE = CodecPacketType.Server.create(
+            Creepers.id("set_cosmetic"),
+            ByteCodec.BOOLEAN.map(ServerboundCosmeticPacket::new, ServerboundCosmeticPacket::shown),
+            NetworkHandle.handle((message, player) -> ServerCosmetics.setCosmeticShown(player, message.shown()))
+    );
 
     @Override
     public PacketType<ServerboundCosmeticPacket> type() {
         return TYPE;
-    }
-
-    private static class Handler extends CodecPacketType<ServerboundCosmeticPacket> implements ServerboundPacketType<ServerboundCosmeticPacket> {
-
-        public Handler() {
-            super(
-                    ServerboundCosmeticPacket.class,
-                    Creepers.id("cosmetic"),
-                    ByteCodec.BOOLEAN.map(ServerboundCosmeticPacket::new, ServerboundCosmeticPacket::shown)
-            );
-        }
-
-        @Override
-        public Consumer<Player> handle(ServerboundCosmeticPacket message) {
-            return player -> ServerCosmetics.setCosmeticShown(player, message.shown());
-        }
     }
 }
